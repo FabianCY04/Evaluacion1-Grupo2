12a84492748e0f0b65085c33cab55921509a784b
CleiberZB <cleiberzarate93@gmail.com>
2026-06-06 18:50:32 -0400

    [ADD]se agrego los mombres de los integrantes y cedula -m integrante Cleiber Zarate V-27866594

--- main.cpp
+++ main.cpp
@@ -9,5 +9,12 @@ int main()
     cout << "Por favor, ingresa tu nombre: ";
     cin >> nombre;
 
+    cout << "Hola, " << nombre << "! Gracias por usar nuestro programa." << endl;
+
+    cout << "Integrantes del Grupo 2 que participaron en esta actividad:" << endl;
+    cout << "--------------------" << endl;
+    cout << "1. Fabian Carapaica C.I: 31000390" << endl;
+    cout << "2. Cleiber Zarate C.I: 27866594" << endl;
+    cout << "--------------------" << endl;
     return 0;
 }
