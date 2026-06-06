09b13da01535776fef5045420db27e8c74bd3969
FabianCY04 <cleiberzarate93@gmail.com>
2026-06-06 18:23:30 -0400

    [ADD]: Añadió el archivo main.cpp integrante: Fabian Carapaica V-31000390

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string nombre;
+    cout << "Bienvenido Este es el programa del Grupo 2" << endl;
+    cout << "Por favor, ingresa tu nombre: ";
+    cin >> nombre;
+
+    return 0;
+}
